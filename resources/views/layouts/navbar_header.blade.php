c94a3ec305d5c9983a77e475f0b895372b64b361
eserranda <eser.randa@gmail.com>
2024-08-25 11:20:32 +0800

    edit route

--- resources/views/layouts/navbar_header.blade.php
+++ resources/views/layouts/navbar_header.blade.php
@@ -60,7 +60,7 @@
 
           <!-- LOGO -->
           <div class="logo-box">
-              <a href="index.html" class="logo text-center">
+              <a href="/" class="logo text-center">
                   <span class="logo-lg">
                       {{-- <img src="{{ asset('assets') }}/images/logo-light.png" alt="" height="16"> --}}
                       <span class="logo-lg-text-light">PKB - GTM</span>
