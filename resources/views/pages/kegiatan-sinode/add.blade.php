a90de4ed411280f560f78e9c07eb6752f7e4590a
eserranda <eser.randa@gmail.com>
2024-08-25 22:51:33 +0800

    reset from

--- resources/views/pages/kegiatan-sinode/add.blade.php
+++ resources/views/pages/kegiatan-sinode/add.blade.php
@@ -121,6 +121,8 @@
                         }
                     });
 
+                    const form = document.getElementById('addForm');
+                    form.reset();
                     $('#datatable').DataTable().ajax.reload();
                     $('#addModal').modal('hide');
                 }
